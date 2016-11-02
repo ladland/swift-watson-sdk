687dc08b9dfffeae1029308564a3d34dd0466658
ladland <ladland@somewhereonline.info>
2016-11-02 13:14:26 +1100

    Update Package.swift
    reorganised file to be compatible with the vapor framework

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "WatsonDeveloperCloud",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14)
+    ],
     targets: [
         Target(name: "RestKit"),
         Target(name: "WeatherCompanyData", dependencies: [.Target(name: "RestKit")]),
@@ -11,11 +15,8 @@ let package = Package(
         Target(name: "Conversation", dependencies: [.Target(name: "RestKit")]),
         Target(name: "AlchemyLanguage", dependencies: [.Target(name: "RestKit")]),        
         Target(name: "AlchemyVision", dependencies: [.Target(name: "RestKit")])
-    ],
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14)
     ]
+    
 )
 
 
